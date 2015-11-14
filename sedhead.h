2c4edd04cbcd38bf9c64e74f5b839a33a1a99f99
James Ross <jamr2@pdx.edu>
2015-11-14 17:30:45 +0000

    fixed a couple bugs, changed a couple macros

--- sedhead.h
+++ sedhead.h
@@ -1,21 +1,13 @@
 /*
     stuff i use for stuff.
+
+
+    I really want to know how i should handle increments in for loops in 
+    my macros.
+        Have them pass an increment variable?
+                         ||
+        Have the mixed declaration and declare it with random __'s.
 */
-
-#ifdef __SED_ERR__
-    #include "err_handle/err_handle.h"  /* error handling functions */
-#endif
-#ifdef __SED_NUM__
-    #include "get_num/get_num.h"        /* convert strings to int types */
-#endif
-
-#ifdef __SED_LINUX__
-    #include <sys/types.h>
-    #include <sys/stat.h>
-    #include <unistd.h>
-    #include <fcntl.h>
-    #include <sys/time.h>
-#endif
 
 #ifndef _SED_HEAD_
 #define _SED_HEAD_
@@ -27,6 +19,24 @@
 #include <getopt.h>
 #include <time.h>
 #include <ctype.h>
+
+/*#define NDEBUG*/
+#include <assert.h>
+
+
+#ifdef __SED_ERR__
+    #include "err_handle/err_handle.h"  /* error handling functions */
+#endif
+#ifdef __SED_NUM__
+    #include "get_num/get_num.h"        /* convert strings to int types */
+#endif
+
+#ifdef __SED_LINUX__
+    #include <sys/stat.h>
+    #include <unistd.h>
+    #include <fcntl.h>
+    #include <sys/time.h>
+#endif
 
 
 #ifndef __FL_CONSTS__
@@ -80,18 +90,15 @@ typedef enum {false, true} Bool;
 
                     /* input */
 /* clears the input buffer using variable char ch; and getchar (). */
-#define clear_buff()                                    \
-{                                                       \
-    char _TR_SH_ = '\0';                                \
-    while(((ch) = getchar()) != '\n' && (ch) != EOF);   \
-} /* end clear_buff */
+#define clear_buff(ch) while(((ch) = getchar()) != '\n' && (ch) != EOF)
 
 /* get 1 character from stdin using getchar, clear buffer afterwards if
    needed */
 #define getChar(input)                \
 {                                     \
+    char _c_h_ = '\0';                \
     if(((input) = getchar()) != '\n'){\
-        clear_buff();}                \
+        clear_buff(_c_h_);}           \
 } /* end getChar */
 
 /* get a single character from stdin and loop untill input is correct 
@@ -106,70 +113,41 @@ typedef enum {false, true} Bool;
     }while(input != 'Y' && input != 'N');                       \
 } /* end getChar_check #}}} */
 
-/* get a line of input from a buffer, clears the buffer if required */
-#define getLineInput(input, max, fpntr, len)\
-{                                           \
-    fgets((input),(max),(fpntr));           \
-    (len) = strlen((input)) - 1;            \
-    if(input[(len)] == '\n'){               \
-        input[(len)] = '\0';}               \
-    else{                                   \
-        clear_buff(); }                     \
+/* get a line of input from a buffer, clears the buffer if required. Input
+   must be dealloced or on the heap for compilation */
+#define getLineInput(input, max, filepntr, len)       \
+{                                                     \
+    char __c_h__ = '\0';                              \
+    if((input) == NULL){                              \
+        (input) = (char*) malloc(sizeof(char)*max);}  \
+                                                      \
+    memset((input), '\0', max);                       \
+    fgets((input),(max),(filepntr));           \
+    (len) = strlen((input)) - 1;               \
+    if(input[(len)] == '\n'){                  \
+        input[(len)] = '\0';}                  \
+    else{                                      \
+        clear_buff(__c_h__); }                 \
 } /* end getLineInput */
 
                     /* other */
 /* create a bit mask for a given range of bits. start, end. (lsb,msb) */
-#define create_mask(start, end, resMask)                                    \
+#define create_mask(increment, start, end, resMask)                         \
 {                                                                           \
-    int32_t ___S_;                                                          \
     if((start) > (end)){                                                    \
         errmsg("create_mask: start > end, no mask was generated.");}        \
                                                                             \
-    resMask = 0;                                                            \
-    for((___S_) = (start); i <= (end); ++(___S_)){                          \
-        (resMask) |= 1 << (___S_);}                                         \
+    resMask = 0; /* just to make sure im not an idiot when i call this */   \
+    for((increment) = (start); i <= (end); ++(increment)){                  \
+        (resMask) |= 1 << increment;}                                       \
 } /* end create_mask */
 
 /* adds all the ascii values in a character array together */
-#define sumArr(array, size, res)                              \
+#define sumChars(increment, array, size, res)                 \
 {                                                             \
-    int32_t ___NC_;                                           \
-    (res) = 0;                                                \
-    for((___NC_) = 0; (___NC_) < (size); ++(___NC_)){         \
-        (res) += (array[(___NC_)]);}                          \
+    for((increment) = 0; (increment) < (size), ++(increment)){\
+        (res) += (array[(increment)]);}                       \
 } /* end sumChars */
-
-/* generated a random string, excluding 0x1 - 0x1f and 0x7f, places it in 
-   (string). mod value implicitly ignores 0x7f. 
-   NOTE: If string is NULL, data will be allocated, set to zero, and 
-         random gets seeded with 0.
-         
-         The seed for gen_string is the string. */
-#define gen_string(string, size)                                        \
-{                                                                       \
-    int32_t __K_U_ = 0;                                                 \
-    int32_t _SUM__ = 0;                                                 \
-    char _N_CH_ = '\0';                                                 \
-                                                                        \
-    if((string) == NULL)                                                \
-    {                                                                   \
-        (string) = (char*) malloc(sizeof(char)*(size));                 \
-        memset((string), '\0', (size));                                 \
-    }                                                                   \
-    sumArr((string), (size), _SUM__);                                   \
-    /* seed random using sumArr */                                      \
-    srandom(_SUM__);                                                    \
-                                                                        \
-    (string)[(size)-1] = '\0'; /* make sure there is a null */          \
-    for((__K_U_) = (size)-2; (__K_U_) > -1; --__K_U_)                   \
-    {                                                                   \
-        do                                                              \
-        {                                                               \
-            _N_CH_ = random() % 0x7f;                                   \
-        }while(_N_CH_ < 0x20 && _N_CH_ > 0x0);                          \
-        (string)[(__K_U_)] = _N_CH_;                                    \
-    }                                                                   \
-} /* end gen_string */
 
 /* vectorizes a function funct, its C99 as fuck tho.
    -Type is the type of pointer used. (VA_ARGS could be void for example.). 
@@ -178,9 +156,9 @@ typedef enum {false, true} Bool;
    -funct only takes in one argument. */
 #define apply_funct(type, funct, ...)                      \
 {                                                          \
-    void *stopper = (int32_t[]){0};                        \
+    void *stopper = (int[]){0};                            \
     type **apply_list = (type*[]){__VA_ARGS__, stopper};   \
-    int32_t __GH_;                                         \
+    int __i_;                                              \
                                                            \
     for(__i_ = 0; apply_list[__i_] != stopper; ++__i_){    \
         (funct)(apply_list[__i_]);}                        \
